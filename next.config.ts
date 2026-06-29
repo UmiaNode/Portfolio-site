b8cf7fb120e6c4ab2903b2b12d60724a47105673
UmiaNode <k.umianode@gmail.com>
2026-06-29 13:25:21 +0900

    Redirect Vercel domain to custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "portfolio-site-rho-rouge.vercel.app",
+          },
+        ],
+        destination: "https://portfolio.umianode.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
